b5938fe419267a16b5d3d350c2ad5212a72c6b64
fuzomntungwa777 <fuzomntungwa3@gmail.com>
2026-07-08 18:35:35 +0000

    Clean race column and create temporary table

--- Bright tv queries.sql
+++ Bright tv queries.sql
@@ -40,9 +40,45 @@ FROM user_profiles;
 
 -- COMMAND ----------
 
-SELECT DISTINCT
-            CASE
-                WHEN Race = 'other' THEN 'Unknown'
-                WHEN Race = 'None' THEN 'Unknown'
-            ELSE Race
-            END AS clean_race;
+---Cleaning race column
+SELECT DISTINCT 
+                CASE 
+                    WHEN race = 'None' THEN 'unknown' ---Replaces the value "None" with "Unknown"
+                    WHEN race = ' ' THEN 'unknown' ---Replace the blanks with "Unknown"
+                ELSE race
+                END AS Clean_race
+FROM user_profiles;
+
+-- COMMAND ----------
+
+---Creating temporary table
+CREATE OR REPLACE TEMPORARY TABLE Subs AS (
+    SELECT 
+        COUNT(DISTINCT UserID0) AS number_of_subs, 
+        TO_DATE(RecordDate2) AS watch_date, 
+        DAYNAME(TO_DATE(RecordDate2)) AS Day_name, 
+        MONTHNAME(TO_DATE(RecordDate2)) AS Month_name, 
+        YEAR(TO_DATE(RecordDate2)) AS event_year, 
+        CASE 
+            WHEN DAYNAME(TO_DATE(RecordDate2)) IN ('Sat', 'Sun') THEN '02. Weekend' 
+            ELSE '01. Weekday' 
+        END AS day_classification 
+    FROM viewership 
+    WHERE UserID0 IS NOT NULL 
+    GROUP BY ALL 
+    ORDER BY watch_date DESC
+);
+
+-- COMMAND ----------
+
+---Checking temporary table
+SELECT *
+FROM subs;
+
+-- COMMAND ----------
+
+SELECT
+    SUM(number_of_subs) AS subs,
+    day_classification
+FROM Subs
+GROUP BY day_classification;
